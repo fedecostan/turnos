7135e33c9ebd4277b09f220d931d727608521f96
fedecostan <costantino.fe@gmail.com>
2018-10-17 15:54:03 -0300

    Implementación LOGIN - Spring Security

--- src/main/webapp/resources/js/controllers/indexControllers.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  'use strict';
-   myapp.controller('IndexController',
-        function($scope, $window) {
-            $scope.login = function(){
-                $window.location.href = '/web/home.html';
-            }
-        })
-}());
